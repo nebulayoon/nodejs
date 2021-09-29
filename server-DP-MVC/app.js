e78de3c65370512e0995aff4fc3ab2f7201f070a
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-09-29 02:26:53 +0000

    [기능추가]: 기본 셋팅 진행

---
+++ server-DP-MVC/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = 3000;
+
+const home = require("./routes/home")
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드
+
+// app.use(express.static(path.join(__dirname, '../react-project/build')));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
+// });
+
+
+// app.get('*', ()  => {
+//   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
+// })
+
+module.exports = app;
