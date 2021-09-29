dec6706dfe1252631cedbed91b5e41e3fdc1c245
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-09-29 04:41:05 +0000

    [기능추가]:nodemon설정 완료

--- server-DP-MVC/app/app.js
+++ server-DP-MVC/app/app.js
@@ -7,10 +7,11 @@ const app = express();
 const PORT = 3000;
 
 const home = require("./src/routes/home")
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드
+app.use(express.static(`${__dirname}/src/public`));
 
 // app.use(express.static(path.join(__dirname, '../react-project/build')));
 
