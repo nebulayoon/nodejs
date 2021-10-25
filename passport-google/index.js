6e262f4bf868cefdf2173947044bc54f8b19c08a
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2021-10-25 22:41:32 +0900

    [기능추가]: OAuth 로그인 및 로그아웃 구현 완료

--- passport-google/index.js
+++ passport-google/index.js
@@ -1,9 +1,17 @@
-const express  = require('express')
+const express  = require('express');
+const session = require('express-session')
 const passport = require('passport')
 
 require('./auth');
 
+const isLoggedIn = (req, res, next) => {
+  req.user ? next() : res.sendStatus(401);
+}
+
 const app = express();
+app.use(session({ secret: 'cats' }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
   res.send('<a href="/auth/google">Authenticate with Google</a>')
@@ -11,8 +19,23 @@ app.get('/', (req, res) => {
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
 
-app.get('/protected', (req, res) => {
-  res.send('Hello!')
+app.get('/google/callback', passport.authenticate('google', {
+  successRedirect: '/protected',
+  failureRedirect: '/auth/failure',
+}))
+
+app.get('/auth/failure', (req, res) => {
+  res.send('something went wrong..')
+})
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`)
 }) 
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  req.session.destroy()
+  res.send('shibal')
+})
 
 app.listen(5000, () => console.log('listening on : 5000'))
